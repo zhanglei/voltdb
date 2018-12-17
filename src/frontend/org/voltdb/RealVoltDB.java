9ae5af388f434265d34d1af49dd3f80d143547ba
Nate Clark <nclark@voltdb.com>
2018-12-17 15:00:42 -0500

    VoltDBInterface: Add getter for ElasticService

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -5058,5 +5058,10 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback, HostM
     public Initiator getInitiator(int partition) {
         return m_iv2Initiators.get(partition);
     }
+
+    @Override
+    public ElasticService geElasticService() {
+        return m_elasticService;
+    }
 }
 
