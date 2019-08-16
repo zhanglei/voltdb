c93b9a42892ebac7cba1bfa89d3c56f16ce88011
victorxgao <36307067+victorxgao@users.noreply.github.com>
2019-08-15 22:45:11 -0400

    Eng-16579 tracing tool improvement - filter (#6396)
    * Add filter functionality but the file dump still has problems. Recordes are less than detected outliers.
    
    * ENG-16579-filter-problem-fixed
    
    * the debug code is cleaned
    
    * Correct some typos in the comments
    
    * a little modification on filter. If filter is turned off, the original functionality of tracing tool do not change
    
    * use bounded buffer for filtered trace events
    
    * dump the witeQueue when it is full
    
    * For the sake of performance, use bounded buffer for filtered trace events, and modified previous design, wrap the codes related to the filter in a class
    
    * unused code blocks are removed
    
    * correct typos
    
    * Revisions are made accoding to Yang's comments 1
    
    * use org.voltcore.utils.Pair
    
    * remove debug code
    
    * Add the a Junit test for VoltTrace
    
    * Modification based on ylu's review comments
    
    Usage:
    To turn on the filter of tracing tool:
    
    @Trace filter [time] : the argument "time" needs to be a positive number of type "double" in Java. Its unit is microseconds. This number can not be negative, otherwise an error message will be thrown.
    
    To turn off the filter of tracing tool:
    @Trace filter 0 : the filter is automatically turned off when the "time" is set to be zero.
    
    Change-Id: I030899f4474a68024f0641f4cf636ed5de0522c3

--- src/frontend/org/voltdb/TraceAgent.java
+++ src/frontend/org/voltdb/TraceAgent.java
@@ -17,13 +17,13 @@
 
 package org.voltdb;
 
+import java.io.File;
+import java.util.Collection;
+
 import org.json_voltpatches.JSONObject;
 import org.voltcore.network.Connection;
 import org.voltdb.client.ClientResponse;
 import org.voltdb.utils.VoltTrace;
-
-import java.io.File;
-import java.util.Collection;
 
 public class TraceAgent extends OpsAgent {
     public TraceAgent()
@@ -66,21 +66,68 @@ public class TraceAgent extends OpsAgent {
         String subselector = "status";
         if (params.toArray().length < 1) {
             return "Incorrect number of arguments to @Trace (expects as least 1, received " +
-                   params.toArray().length + ")";
+                   numOfParams + ")";
         }
-        if (params.toArray().length >= 1) {
-            Object first = params.toArray()[0];
-            if (!(first instanceof String)) {
-                return "First argument to @Trace must be a valid STRING selector, instead was " +
-                       first;
+
+        Object first = paramsArray[0];
+        if (!(first instanceof String)) {
+            return "First argument to @Trace must be a valid STRING selector, instead was " +
+                   first;
+        }
+
+        subselector = (String)first;
+
+        // Usage: "exec @Trace status"
+        // Check the status of VoltDB tracing tool and the result is returned.
+        // Usage: "exec @Trace dump"
+        // Write the trace events recorded in the buffer to file, and the absolute path of the file is returned
+        if ("status".equalsIgnoreCase(subselector) || "dump".equalsIgnoreCase(subselector)) {
+            if (numOfParams != 1) {
+                return "Incorrect number of arguments to @Trace " +
+                        subselector + " (expected: 1,  received: " +
+                        numOfParams + ")";
             }
-            subselector = (String)first;
-            if (!(subselector.equalsIgnoreCase("enable") ||
-                  subselector.equalsIgnoreCase("disable") ||
-                  subselector.equalsIgnoreCase("status") ||
-                  subselector.equalsIgnoreCase("dump"))) {
-                return "Invalid @Trace selector " + subselector;
+
+            obj.put("subselector", subselector);
+            obj.put("interval", false);
+            return null;
+        }
+
+        // Enable/Disable the category for VoltDB tracing tool
+        // The tracing tool is turned on automatically when, at least, one category is enabled.
+        // The tracing tool is turned off automatically when there is no enabled category.
+        // Usage: "exec @Trace enable [Category]"
+        // Usage: "exec @Trace disable [Category]"
+        if ("enable".equalsIgnoreCase(subselector) || "disable".equalsIgnoreCase(subselector)) {
+            if (numOfParams != 2) {
+                return "Incorrect number of arguments to @Trace " +
+                        subselector + " (expected: 2,  received: " +
+                        numOfParams + ")";
             }
+
+            // check the validity of argument after enable/disable
+            String categoryItem = paramsArray[1].toString();
+            for (VoltTrace.Category cat : VoltTrace.Category.values()) {
+                if (cat.toString().equalsIgnoreCase(categoryItem)) {
+                    obj.put("subselector", subselector);
+                    obj.put("categories", categoryItem);
+                    obj.put("interval", false);
+                    return null;
+                }
+            }
+
+            // Disable all enabled categories and shutdown the VoltDB tracing tool
+            // Usage: "exec @Trace disable ALL"
+            if ("ALL".equalsIgnoreCase(categoryItem)) {
+                obj.put("subselector", subselector);
+                obj.put("categories", categoryItem);
+                obj.put("interval", false);
+                return null;
+            }
+
+            return "Second argument to @Trace " +
+                    subselector + " must be a valid STRING category or 'ALL', instead was " +
+                    categoryItem;
         }
         // Would be nice to have subselector validation here, maybe.  Maybe later.
         obj.put("subselector", subselector);
@@ -89,13 +136,45 @@ public class TraceAgent extends OpsAgent {
         }
         obj.put("interval", false);
 
-        return null;
+        // Trace event filter of VoltDB Tracing tool
+        // Usage: "exec @Trace filter [filterTime]"
+        // The filter is turned on automatically when filterTime is positive.
+        // Usage: "exec @Trace filter 0"
+        // The filter is turned off automatically by setting the filterTime to be zero.
+        if ("filter".equalsIgnoreCase(subselector)) {
+            if (numOfParams != 2) {
+                return "Incorrect number of arguments to @Trace " +
+                        subselector + " (expected: 2,  received: " +
+                        numOfParams + ")";
+            }
+            // filterTime is a latency target set by the customers
+            // It is used as the threshold to filter the trace events,
+            // whose latencies are larger than the filterTime.
+            String filterTime = paramsArray[1].toString();
+            try {
+                double time = Double.parseDouble(filterTime);
+                if (time < 0) {
+                    return "Second argument of @Trace filter must be a non-negative numeric number";
+                }
+            } catch (NumberFormatException | NullPointerException e) {
+                return "Incorrect type of second argument of @Trace filter " +
+                        filterTime + " (It must be a double-precision number)";
+            }
+
+            obj.put("subselector", subselector);
+            obj.put("filterTime", filterTime);
+            obj.put("interval", false);
+            return null;
+        }
+
+        return "Invalid @Trace selector " + subselector;
     }
 
     @Override
     protected void handleJSONMessage(JSONObject obj) throws Exception
     {
-        VoltTable[] results = new VoltTable[] {new VoltTable(new VoltTable.ColumnInfo("STATUS", VoltType.STRING))};
+        VoltTable[] results = new VoltTable[] {new VoltTable(new VoltTable.ColumnInfo("STATUS", VoltType.STRING)),
+                                               new VoltTable(new VoltTable.ColumnInfo("FILTER", VoltType.STRING))};
 
         OpsSelector selector = OpsSelector.valueOf(obj.getString("selector").toUpperCase());
         if (selector != OpsSelector.TRACE) {
@@ -113,13 +192,30 @@ public class TraceAgent extends OpsAgent {
         } else if (subselector.equalsIgnoreCase("enable")) {
             VoltTrace.enableCategories(VoltTrace.Category.valueOf(obj.getString("categories").toUpperCase()));
         } else if (subselector.equalsIgnoreCase("disable")) {
-            VoltTrace.disableCategories(VoltTrace.Category.valueOf(obj.getString("categories").toUpperCase()));
+            if (obj.getString("categories").equalsIgnoreCase("all")) {
+                // disable all categories and close the tracer
+                VoltTrace.disableAllCategories();
+            } else {
+                VoltTrace.disableCategories(VoltTrace.Category.valueOf(obj.getString("categories").toUpperCase()));
+            }
+        } else if (subselector.equalsIgnoreCase("filter")) {
+            double time = Double.parseDouble(obj.getString("filterTime"));
+            if (time > 0) {
+                VoltTrace.turnOnFilter(time);
+            } else {
+                VoltTrace.turnOffFilter();
+            }
         } else if (subselector.equalsIgnoreCase("status")) {
             final Collection<VoltTrace.Category> enabledCategories = VoltTrace.enabledCategories();
             if (enabledCategories.isEmpty()) {
                 results[0].addRow("off");
             } else {
                 results[0].addRow(enabledCategories.toString());
+            }
+            if (VoltTrace.isFilterOn()) {
+                results[1].addRow("On");
+            } else {
+                results[1].addRow("Off");
             }
         }
 
