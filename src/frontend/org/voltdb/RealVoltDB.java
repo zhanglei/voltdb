a0c683111c690447833d58c55142f513ca35483e
Jenkins CI <test@voltdb.com>
2019-09-06 05:35:27 -0400

    Set Version x.y.BoCPerfTrace

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -263,9 +263,9 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback, HostM
     // Cluster settings reference and supplier
     final ClusterSettingsRef m_clusterSettings = new ClusterSettingsRef();
     private String m_buildString;
-    static final String m_defaultVersionString = "9.1.BoCPerfTrace";
+    static final String m_defaultVersionString = "x.y.BoCPerfTrace";
     // by default set the version to only be compatible with itself
-    static final String m_defaultHotfixableRegexPattern = "^\\Q9.1.BoCPerfTrace\\E\\z";
+    static final String m_defaultHotfixableRegexPattern = "^\\Qx.y.BoCPerfTrace\\E\\z";
     // these next two are non-static because they can be overrriden on the CLI for test
     private String m_versionString = m_defaultVersionString;
     private String m_hotfixableRegexPattern = m_defaultHotfixableRegexPattern;
